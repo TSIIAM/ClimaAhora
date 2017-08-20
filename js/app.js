b3f96c86bf2fc946cb79609fde0119860371b5e2
TSIIAM <tsi.iamesa@gmail.com>
2017-08-20 20:16:16 -0300

    done

--- js/app.js
+++ js/app.js
@@ -28,25 +28,51 @@ function displayLocation(latitude,longitude){
  			var ciudad = address.address_components[2]["long_name"];
 		 	var direccion = address.formatted_address.split(",");
 		 	var pais = direccion[2];
-			$("#ciudad").text(ciudad);
-			$("#pais").text(pais);	
+			$("#lugar ").text(ciudad + " " + pais);
+ 
+			var d = new Date();
+			var m = d.getDate();
+			var n = weekday[d.getDay()];
+			$("#fecha").text(n + " " + m);
 		}
 	};
 	request.send();
 };
- 
+
+var weekday = new Array(7);
+weekday[0] =  "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+
+ function Weather(temperatura,descripcion,icono){
+ 	this.temperatura = temperatura;
+ 	this.descripcion = descripcion;
+ 	this.icono  = icono;
+ }
+
 function searchWeather(latitude,longitude){
  
 		var http = new XMLHttpRequest();
-		var APIKEY = "848ef7adaf8ccc408f95c89cb032bfbf";
-		var url  =  `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID={APIKEY}`;
+		var url  =  `https://fcc-weather-api.glitch.me/api/current?lat=${latitude}&lon=${longitude} `;
 		var method = "GET";
 
-		http.open(method, url);
+		http.open(method, url,true);
 		http.onreadystatechange = function(){
 			if(http.readyState  == XMLHttpRequest.DONE && http.status === 200 ){
 				var data = JSON.parse(http.responseText);
-				console.log(data);	
+				console.log(data);
+				var temperatura = data.main.temp;
+				var descripcion = data.weather[0].description;
+				var icono =  data.weather[0].icon;
+				myWeather = new Weather(temperatura,descripcion,icono);
+				updateWeather(myWeather);
+				console.log(icono);
+				
 			}else if(http.readyState === XMLHttpRequest.DONE){
 				alert("Algo no funciona.");
 			}
@@ -55,6 +81,34 @@ function searchWeather(latitude,longitude){
 		http.send();
 	}
 
+function updateWeather(weather){
+				$("#temperatura").text(weather.temperatura + "°c");
+				$("#descripcion").text(weather.descripcion);
+				$("#icono").html(`<img src="${weather.icono}">`);
 
+				$("#temperatura").on("click",changeMetric);
 
+}
+
+function change(){
+ 	var celsius = true;
+	return function(){
+			var temp = myWeather.temperatura;
+				if(celsius){
+					var f = 1.8 *  temp + 32 ;
+					myWeather.temperatura = f;
+					$("#temperatura").text(f +"°f");
+					celsius = false;
+					console.log(f, myWeather.temperatura);
+				}else{
+					console.log(temp);
+					var c =  (temp - 32) / 1.8  ;
+					myWeather.temperatura = c;
+					$("#temperatura").text(c +"°c");
+					celsius = true;
+				}
+
+	}
+};
+var changeMetric = change();
 
